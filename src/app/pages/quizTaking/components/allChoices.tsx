0f76e3279779414fafca3145af1cefeb4fea535e
Kreliannn <krelianquimson@gmail.com>
2025-04-04 19:07:20 +0800

    add quiz taking section and all choices feature

---
+++ src/app/pages/quizTaking/components/allChoices.tsx
@@ -0,0 +1,67 @@
+
+import { useState, useEffect } from "react"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+import { Trash, X } from "lucide-react";
+import { reviewerInterface } from "@/app/interface/reviewer"
+import Navbar from "@/components/ui/quizNavbar"
+import { Send } from 'lucide-react';
+import { Badge } from "@/components/ui/badge"
+
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+
+
+
+export default function AllChoices({ quiz, setInput, input } : { quiz : reviewerInterface[], setInput : React.Dispatch<React.SetStateAction<string>>, input : String } )
+{
+
+    return(
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button variant="black"> Choices </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+      <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle> Choices</DrawerTitle>
+            <DrawerDescription>you can click the answer.</DrawerDescription>
+          </DrawerHeader>
+          <div className="h-auto  max-h-72 md:max-h-62 p-4 pb-0 flex flex-wrap items-start gap-2 overflow-auto">
+            {
+                quiz?.map((item, index) => {
+                    return (
+                        <Badge 
+                         variant={(input != item.item)? "outline" : "default"}
+                         key={index}
+                         onClick={() => setInput(item.item)}
+                        > 
+                            {item.item} 
+                        </Badge >
+                    )
+                })
+            }  
+          </div>
+
+          <DrawerFooter className="w-full">
+            <DrawerClose asChild className="w-full">
+              <Button variant="black" className="w-full">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>     
+      </DrawerContent>
+    </Drawer>
+    )
+
+}
--- src/app/pages/quizTaking/page.tsx
+++ src/app/pages/quizTaking/page.tsx
@@ -6,23 +6,71 @@ import { Textarea } from "@/components/ui/textarea"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 import { Trash, X } from "lucide-react";
+import { reviewerInterface } from "@/app/interface/reviewer"
 import Navbar from "@/components/ui/quizNavbar"
+import { Send } from 'lucide-react';
+import AllChoices from "./components/allChoices"
+
+
 
 export default function TakeQuiz()
 {
-    const [search, setSearch] = useState("")
-
     const reviewer = useReviewerStore((state) => state.reviewer)
     
+    const [quiz, setQuiz] = useState<reviewerInterface[]>()
+    const [input, setInput] = useState<string>("")
 
+    useEffect(() => {
+        setQuiz(reviewer)
+    },[])
+
+    const checkIfExists = ( item: string) => {
+        if(quiz) return quiz.some((obj) => obj.item.toUpperCase() === item.toUpperCase()); 
+    }
+    
+    const submit = () => {
+
+    }
 
     return(
-        <div className="w-full bg-stone-100 ">
-            <Navbar />
-            
-            
+        <div className="w-full h-dvh bg-stone-100 ">
+            <Navbar />         
+            <div className="w-5/6  h-auto m-auto s ">
 
-            <br />
+                <div className="grid grid-cols-2 h-10   gap-1.5 p-2 ">
+                    <div className="">
+                        <h1 className="text-left md:text-2xl text-lg font-bold text-stone-700"> Quiz Name </h1>
+                    </div>
+                    <div className="w-100">
+                        <h1 className="text-right md:text-2xl text-lg font-bold text-stone-700"> Item Left: 5 </h1>
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-1 h-64 rounded    gap-1.5 p-2 ">
+                    <div className=" h-full bg-white rounded shadow-md p-2">
+
+                    </div>  
+                </div>
+
+                <div className="grid grid-cols-4 w-full items-center gap-1.5 p-2">
+                    <Input 
+                        type="text" 
+                        className={`col-span-3 h-14 bg-white shadow-lg font-bold text-lg ${(checkIfExists(input)) ? "text-green-700" : ""}`}
+                        placeholder="Answer........." 
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <Button className="col-span-1 h-14 shadow-lg font-bold text-lg flex items-center gap-2" variant="black">
+                        <Send className="" />  
+                        <span className="hidden md:block">Submit</span>  
+                              
+                    </Button>
+                </div>
+
+                <AllChoices quiz={reviewer} setInput={setInput} input={input} />
+
+
+            </div>
         </div>
     )
 
